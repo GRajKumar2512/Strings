71fa8702b60e08cc9c5db9a1cddb1648dbca7082
G. Raj Kumar <grajkumar2512@gmail.com>
2023-10-03 16:36:33 +0530

    Initial commit

---
+++ src/Lexicographical_frequency.java
@@ -0,0 +1,16 @@
+public class Lexicographical_frequency {
+    // Question: Print frequencies characters (in sorted order) in a string of lower case alphabets.
+    public static void main(String[] args) {
+        String str = "geeksforgeeks";
+        int[] count = new int[26];
+        for(int i = 0; i < str.length(); i++){
+            //access the array index of the character using its ascii value
+            count[(int)str.charAt(i) - 97]++;
+        }
+        for(int i = 0; i < count.length; i++){
+            if(count[i] > 0){
+                System.out.print((char)(i + 97) +" = " + count[i] +", ");
+            }
+        }
+    }
+}
